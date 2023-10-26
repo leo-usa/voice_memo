dff8f1301f2c9f9dd622de9d398584ede97be73c
Aaljani <janicea@metropolia.fi>
2023-10-26 19:37:45 +0300

    whisper api attempt

---
+++ lib/API/whisper_api.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'api_key.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+final Uri whisperUrl =
+    Uri.parse("https://api.openai.com/v1/audio/transcriptions");
+final Uri promptUrl = Uri.parse("https://api.openai.com/v1/chat/completions");
+
+final Map<String, String> headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer $apiKey',
+};
+
+class WhisperRequest {
+  File? file;
+  final String model = "whisper-1";
+
+  WhisperRequest(File file) {
+    this.file = file;
+  }
+}
+
+Future<String?> requestWhisper(File file) async {
+  try {
+    WhisperRequest request = WhisperRequest(file);
+    if (file == null) {
+      return null;
+    }
+    http.Response response = await http.post(
+      whisperUrl,
+      headers: headers,
+      body: json.encode(request),
+    );
+    String chatResponse = response.text;
+    return chatResponse;
+  } catch (e) {
+    print("error $e");
+  }
+  return null;
+}
